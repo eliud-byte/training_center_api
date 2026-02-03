742049c2328bcaa0c779b4a5675de3496d21fb6c
Eliud Mwaura <eliudmwaura101@gmail.com>
2026-02-03 21:52:21 +0300

    feat: implement custom user model with roles

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+# This allows you to view/edit users in the browser
+admin.site.register(User, UserAdmin)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    # Options for role field
+    class Role(models.TextChoices):
+        ADMIN = "ADMIN", "Admin"
+        INSTRUCTOR = "INSTRUCTOR", "Instructor"
+        STUDENT = "STUDENT", "Student"
+
+    base_role = Role.ADMIN
+
+    role = models.CharField(max_length=50, choices=Role.choices)
+
+    # We can add more profile fields here later if needed
+
+    def save(self, *args, **kwargs):
+        # If no role is set, default to the base_role
+        if not self.pk and not self.role:
+            self.role = self.base_role
+        return super().save(*args, **kwargs)
+
