e5e3606492aa1730494b836b5a8469e3345bb388
Eliud Mwaura <eliudmwaura101@gmail.com>
2026-02-12 14:32:32 +0300

    feat: enhance cohort serializer readability and expose user roles in admin

--- courses/serializers.py
+++ courses/serializers.py
@@ -7,6 +7,20 @@ class CourseSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class CohortSerializer(serializers.ModelSerializer):
+    # These fields are "Read Only" - they appear in GET responses but aren't required for POST
+    course_name = serializers.ReadOnlyField(source='course.title')
+    instructor_name = serializers.ReadOnlyField(source='instructor.username')
+
     class Meta:
         model = Cohort
-        fields = '__all__'
+        fields = [
+            'id',
+            'name',
+            'course',
+            'course_name',  # <--- distinct field for the name
+            'instructor',
+            'instructor_name',  # <--- distinct field for the username
+            'start_date',
+            'end_date',
+            'capacity',
+        ]
--- users/admin.py
+++ users/admin.py
@@ -3,4 +3,17 @@ from django.contrib.auth.admin import UserAdmin
 from .models import User
 
 # This allows you to view/edit users in the browser
-admin.site.register(User, UserAdmin)
+class CustomUserAdmin(UserAdmin):
+    # 'fieldsets' controls the "Change User" page
+    fieldsets = list(UserAdmin.fieldsets) + [
+        ('Custom Fields', {'fields': ('role',)}),
+    ]
+
+    # 'add_fieldsets' controls the "Add User" page
+    add_fieldsets = list(UserAdmin.add_fieldsets) + [
+        ('Custom Fields', {'fields': ('role',)}),
+    ]
+    # 'list_display' controls the columns in the user list
+    list_display = list(UserAdmin.list_display) + ['role']
+
+admin.site.register(User, CustomUserAdmin)
